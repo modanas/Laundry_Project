33f61553d52abbdafdcf95c41506c3101ae32a0d
modanas <mdanasjawed2601@gmail.com>
2025-08-31 17:07:11 +0530

    Fix CSS module import case issue

--- src/components/signup/SignUpForm.jsx
+++ src/components/signup/SignUpForm.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { FaFacebook, FaGoogle } from "react-icons/fa";
 import { motion } from "framer-motion";
-import styles from "./Signupform.module.css"// <--- ADD THIS
+import styles from "./signupform.module.css"// <--- ADD THIS
 
 
 const SignUpForm = () => {
